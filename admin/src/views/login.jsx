f3dd31e4abb56d9471a96f46e00172bdaf92eda8
kuangjianming <kjm1513758279@qq.com>
2022-03-01 09:47:56 +0800

    初始化页面

---
+++ admin/src/views/login.jsx
@@ -0,0 +1,18 @@
+import {Component} from "react";
+class Login extends Component{
+    constructor(props){
+        super(props)
+        this.state={
+
+        }
+    }
+    render(){
+        return(
+            <div>
+                登陆页面
+            </div>
+        )
+    }
+}
+
+export default Login
